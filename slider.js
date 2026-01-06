81fb61d50766c66a4ffe41d8feb7d87b53ed05a2
syuns17 <msrk71114002@iCloud.com>
2026-01-06 17:13:32 +0900

    event WS azuki kogin

--- slider.js
+++ slider.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".slider").forEach(slider => {
-      const images = slider.querySelectorAll("img");
-      const prev = slider.querySelector(".slide-btn.prev");
-      const next = slider.querySelector(".slide-btn.next");
+        const images = slider.querySelectorAll("img");
+        const prev = slider.querySelector(".slide-btn.prev");
+        const next = slider.querySelector(".slide-btn.next");
 
-      let index = 0;
-      images[index].classList.add("active");
+        let index = 0;
+        images[index].classList.add("active");
 
-      prev.addEventListener("click", () => {
+        prev.addEventListener("click", () => {
         images[index].classList.remove("active");
         index = (index - 1 + images.length) % images.length;
         images[index].classList.add("active");
-      });
+        });
 
-      next.addEventListener("click", () => {
+        next.addEventListener("click", () => {
         images[index].classList.remove("active");
         index = (index + 1) % images.length;
         images[index].classList.add("active");
-      });
+        });
     });
 
-  });
+});
